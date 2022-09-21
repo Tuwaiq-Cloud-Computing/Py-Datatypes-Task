59f296a16f4057c4fac72f8d4231816f8506fb24
EbraheemFahad <114017154+EbraheemFahad@users.noreply.github.com>
2022-09-21 05:32:15 -0700

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+#list = [2, 0, 1, 0]
+#Question 1 The answer is print(len(list))
+# print(len(list))
+
+#Question 2 - The answer is 2
+# print(list[0])
+
+#Question 3 - The answer is 2
+# print(list.count(0))
+
+#Question 4 - The answer is  list index out of range
+#print(list[4])
+
+#Question 5 - The answer is  Null
+# 2 in list
+
+#Question 6 - The answer is list.append('A') Then print (list) 
+# list.append('A')
+# print (list)
+
+#Question 7 - The answer is list.sort() Then print (list) 
+# list.sort()
+# print(list)
+
+#Question 8 - The answer is list.sort() Then print (list) 
+#list.pop(0)
+#print(list)
+
+#Question 9 - The answer is list.sort() Then print (list) 
+# list.pop(0)
+# list.pop(2)
+# print(list)
+
+#Question 10 - The answer is list.sort() Then print (list) 
+#list.reverse()
+#print(list)
