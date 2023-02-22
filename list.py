51809592f78065feaa8c89642fde9d54ec62ce0e
Abdulmajeed1414 <122810105+Abdulmajeed1414@users.noreply.github.com>
2023-02-22 23:53:25 +0300

    Abdulmajeed I Alnawfal

---
+++ list.py
@@ -0,0 +1,42 @@
+list = [2, 0, 1, 0]
+
+# Output: 4
+# Code: print(len(list))
+
+# Output: 2
+# Code: print(list[0])
+
+# Output: 2
+# Code: print(list.count(0))
+
+# Output: error dou to no value in index 4
+# Code: print(list[4])
+
+# Output: ?
+#   2 in list
+
+# Output: [2, 0, 1, 0, 'A']
+# Code: list.insert(4, "A")
+# print(list)
+
+
+
+# Output: [0, 0, 1, 2]
+# (list.sort())
+# print(list)
+
+
+# Output: [2, 0, 1]
+# Code: del list[3]
+# print(list)
+
+
+# Output: [0, 1]
+# list.pop(0)
+# list.pop(2)
+# print(list)
+
+
+# Output: [0, 1, 0, 2]
+# list.reverse()
+# print(list)
